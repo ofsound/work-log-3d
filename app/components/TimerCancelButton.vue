1edcd75f735e7735c8a47e3a87eddabacbbe2209
Ben Montgomery <ben@modernthings.net>
2026-03-23 20:12:31 -0600

    make the cancel button hitzone larger

--- app/components/TimerCancelButton.vue
+++ app/components/TimerCancelButton.vue
@@ -2,7 +2,7 @@
 
 <template>
   <button
-    class="absolute -top-3 -left-3 cursor-pointer rounded-full border-2 border-button-secondary-border bg-button-secondary p-1 text-button-secondary-text shadow-control hover:border-border-strong"
+    class="absolute -top-3 -left-3 cursor-pointer rounded-full border-2 border-button-secondary-border bg-button-secondary p-1 text-button-secondary-text shadow-control before:absolute before:-inset-[6px] before:rounded-full before:content-[''] hover:border-border-strong"
   >
     <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 100 100">
       <line x1="10" y1="10" x2="90" y2="90" stroke="currentColor" stroke-width="20" />
